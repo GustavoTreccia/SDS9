cd6b05d38e56d7d5a35e126654ebf4ad9a2079a4
GustavoTreccia <g.treccia@outlook.com>
2022-07-16 18:10:45 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,8 +18,12 @@ function SalesCard() {
     const [maxDate, setMaxDate] = useState(max);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(response => { setSales(response.data.content) })
-    }, []);
+
+        const dmin = minDate.toISOString().slice(0, 10)
+        const dmax = maxDate.toISOString().slice(0, 10)
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => { setSales(response.data.content) })
+    }, [minDate, maxDate]);
 
     return (
         <>
@@ -74,7 +78,7 @@ function SalesCard() {
                                             </td>
                                         </tr>
                                     )
-                                })};
+                                })}
                         </tbody>
 
                     </table>
